392cc20f8fa2761d0502c21ece2964c3ce795416
BakhromovDev <bahramovhusein@gmail.com>
2023-09-05 17:24:35 +0500

    Homework

---
+++ app.js
@@ -0,0 +1,697 @@
+// // masala 1
+
+// let k = 16;
+// let r = 8;
+
+// console.log(k==r)
+
+// // masala 2
+
+
+// let w = 8%4;
+// let e = 4;
+// let f =10;
+
+// console.log(w==f);
+
+// // masala 3
+
+// let g = 16;
+// let h = 8;
+
+// g = g ** h;
+// console.log(g);
+
+// // masala 4
+
+// let a = 2
+// let p = 2 * 4;
+// console.log(p); 
+
+// //masala 5
+
+// let n = 2
+// let s = 2 ** 4;
+// console.log(s); 
+
+// // masala 6
+
+// let l = 3.14 * 5;
+// let d = 5;
+// console.log(l);
+
+
+
+// masala 1
+// let age = 18;
+// age++
+// let young = `Mening yoshim ${age}`
+
+// console.log(young);
+
+// //  masala 2
+
+// let x = 10;
+// let y = 5;
+// let result = x>y ? "x":"y";
+// console.log("Shu son katta" , result);
+
+// masala 3
+
+// let x = 5;
+// let y = 7;
+// let result = x>=y ? "true":"false";
+// console.log(result);
+
+
+
+// let bool = false ;
+//let result = bool ? "rost":"yolg'on";
+//console.log(result);
+
+
+// let x = 10;
+// let y = 5;
+// let result = x>y ? "x":"y";
+// console.log("Shu son katta" , result);
+
+
+
+
+
+// -==--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+
+
+
+// let dictonary_eng = prompt("write word")
+// const dictonaryEng = document.querySelector(".dictonary_eng");
+//  dictonaryEng.textContent = dictonary_eng;
+
+// switch (dictonary_eng)
+//     {
+//         case"olma":
+//             dictonaryEng.textContent = "Olma yeyman"
+//             console.log("Olma");
+//             break;
+
+//         case"nok":
+//             dictonaryEng.textContent = "Nok yeyman"
+//             console.log("Nok");
+//             break;
+
+//         case"banan":
+//             dictonaryEng.textContent = "Banan yeyman"
+//             console.log("Banan");
+//             break;
+
+//             default:
+//                 console.log("meva tanlanmadi");
+//     }
+
+    // let age = prompt("Mark your age")
+    // const indicateNum = document.querySelector(".content");
+    // let result = age >= 18 ? "mumkin":"mumkinmas";
+    // indicateNum.textContent = result ;
+
+    // switch (results)
+    // {
+    //     case"18":
+    //     // resultS.textContent = "kirish mumkin"
+    //         console.log("age1");
+    //         break;
+
+    //     case"16":
+    //         // resultS.textContent = "kirish mumkin emas"
+    //         console.log("age2");
+    //         break;
+
+
+    //         default:
+    //             console.log("Yosh tanlanmadi");
+    // }
+
+
+
+    // let number = prompt("Mark your age")
+    // const answer = document.querySelector(".answer");
+    // number.textContent = number;
+
+    // let age1 = 17;
+    // let age2 = 11;
+
+    // let result = age1 >age2 ? "mumkin":"mumkin emas";
+    // console.log( result );
+
+    // let number = prompt("write your age");
+    // const answer = document.querySelector(".answer");
+    // number.textContent = number;
+
+    // let age1 = 19;
+    // let age2 = 17;
+
+    // let result = age1 >age2 ? "mumkin":"mumkin emas";
+    // console.log( result);
+    
+    // // console.log(number);
+
+
+
+
+    
+
+    // switch (number) {
+    //     case "81":
+    //         console.log("Javob to'g'ri");
+    //         break;
+    
+    //     default:
+    //         console.log("topa olmadingiz");
+    //         break;
+    // }
+
+
+    // +------++-+++--+-+-+-+-+-+-+-+-+-+-+-+++++//
+    // for (let item = 0; item <= 10; item ++) {
+    //     console.log(item);
+    // }
+
+
+    // let shop = "banan"
+    // if(shop == 'olma'){
+    //     console.log(olaman);
+    // }
+    // else
+    // {
+    //     console.log("olmayan");
+    // }
+
+
+    // let pupil = 
+    // {
+    //     name: "Asliddin",
+    //     age: "7" ,
+    //     school: "11 maktab"
+
+    // };
+
+    // if (pupil.age >= 8)
+    // {
+    //     console.log("yosh meyyordan ortiq");
+    // } else if (pupil.age >= 7){
+    //     console.log("yosh meyyori yertarli");
+    // }else if (pupil.age >= 7){
+    //     console.log("yosh meyyori yrtarli emas");
+    // }
+
+    // let age = prompt ("Yoshingizni kiriting")
+
+    // const yosh = document.querySelector(".age");  
+    // if (age >= 18) {
+    // console.log("Saytga kirishingiz mumkin");
+    // yosh.textContent =  "Saytga kirishingiz mumkin"
+    // } else  {
+    //     console.log("Saytga kirishingiz mumkin emas");
+    //     yosh.textContent =  "Saytga kirishingiz mumkin emas"
+    // } 
+
+
+
+
+
+
+
+
+
+
+    // =====================================obyectlar======================================================
+
+
+
+
+    // ++--++classwork-01++--++
+
+    // let user = {
+    //     name : "SultonAli",
+    //     age : 18,
+    //     job : "student"
+    // }
+
+    // console.log(user.name);
+    // console.log(user["job"]);
+
+
+    // classwork-02
+
+
+
+    // let pupils = ['Asliddin',' SultonAli', 'Ulugbek' , 'Zuhra']
+    // //pupils.length;
+    // // pupils.push('Husan');
+    // // pupils.pop();
+    // // pupils.shift(); 
+    // // pupils.unshift(Munisa); 
+
+    // console.log(pupils);
+
+    
+
+    // classwork-03
+
+    // let pupils = ['Asliddin',' SultonAli', 'Ulugbek' , 'Zuhra']
+    // let pupils = length
+    // let pupils = array[0]
+    // pupils.pop();
+    // pupils.unshift('Zuhra'); 
+    // console.log(pupils);
+
+
+
+    // array mavzusi uyga vazifalari
+
+
+
+
+    
+    // homework-01
+
+    // let array = [14,58,69,89,55]
+    // let num1 = array[0]
+    // let result = (num1 % 3) ? "number.shift":"number.unshift"  ;
+    // console.log(array , result);
+  
+    //homework-02
+
+    // let pupils = ['Olma','Anjir', 'Anor' , 'Uzum']
+    // console.log(pupils.length);
+
+    //homework-03
+
+    // let numbers = [14,58,69,89,45,55,36,96,67,]
+    // let num1 = numbers[0]
+    // let numLast = numbers[numbers.length - 1];
+    // let result = (num1 + numLast) ** 2;
+    // console.log("Answers" , result);
+
+
+// classwork-01
+
+// let car = {
+//     name: 'NissanGTR',
+//     fuelType: 'Benzin',
+//     years: 1998 , //2023 bilan 1990
+//     color : 'white',
+//     color : 'black',
+//     color : 'blue',//black or white
+//     price : 7800
+// }
+
+// if (car.fuelType = 'metan' &&  car.fuelType <= 'metan') {
+//     console.log(car.fuelType , "- Yonilg'isida foydalangani sababli sport uchun yaroqli" );
+// } else {
+//    console.log(car.fuelType ,"- Yonilg'isidan foydalangani sababli sport uchun yaroqsiz"  );
+// }
+
+//  if (car.price <= 10000) {
+//      console.log(car.price , "- Narxda chiqarilgani sababli sport uchun yaroqli" );
+//  } else {
+//     console.log(car.price ,"- Narxda chiqarilgani sababli sport uchun yaroqsiz"  );
+//  }
+
+// if (car.color >= black  || car.color <= blue) {
+//     console.log(car.color , "- Yilda chiqarilgani sababli sport uchun yaroqli" );
+// } else {
+//     console.log(car.color ,"- Yilda chiqarilgani sababli sport uchun yaroqsiz"  );
+// }
+
+// if (car.years > 1990 &&  car.years < 2000) {
+//     console.log(car.years , "- Yilda chiqarilgani sababli sport uchun yaroqli" );
+// } else {
+//     console.log(car.years ,"- Yilda chiqarilgani sababli sport uchun yaroqsiz"  );
+// }
+
+
+
+
+
+
+// if alse hoeworks
+
+    // work-01
+
+    // x =  5;
+
+    // if (5>0){
+    //     x++
+    // }else{
+    //     5<0
+    // }
+    // console.log(x);
+
+    // work-02
+
+    // x =  5;
+
+    // if (-5>0){
+    //     x++
+    // }else{
+    //     x--
+    // }
+    // console.log(x);
+    
+    // indicator = 5
+
+    // if (indicator>0){
+    //     indicator++
+        
+    // }else if( indicator == 0){
+    //     indicator = 10
+    // }
+    // else{
+    //     indicator - 2
+    // } 
+
+    // console.log(indicator);
+
+
+    // work-03
+
+
+    // let a = 10;
+    // let b = 5;
+
+
+    // if (a>b){
+    //     console.log( a, "ushbu son katta");
+    // }else if(a<b){
+    //     console.log( b , "ushbu son katta");
+    // }
+    
+    
+    //work-04
+
+
+    // let x = 20;
+    // let y = 15;
+
+    // if (x>y){
+    //     console.log( "kichik son" , y ,',' , "tartib raqam -" , y);
+    // }
+
+    //work-05
+
+    // let a = 10;
+    // let b = 5;
+
+    // if (a>b){
+    //     console.log( a , b );
+    // }else if (a<b){
+    //     console.log( b , a );
+    // }
+
+
+    // console.log(result  ,"ushbu son kichik");
+
+
+    //Masala hamma ishlab kelsin
+
+// Masala matni
+
+// Ajoyib masala
+
+// Tug'ilgan yilingizdan hozirgi vaqtdagi yilni ayirib tashlab yoshingiz hozir nechida ekanligini aniqlang
+// va Maktabga borgan yilingizni yozing , va hozir nechanchi sinf ekanligingizni chiqaring
+// endi masalani qiziq joyiga o'tamiz , siz maktabda o'qigan yillarni ichidan bironta yilni kiritilsa sizni usha yili nechanchi sinf 
+// bo'lganingizni chiqarib bersin va maktabga bormagan yilliringiz kiritsa u yilda maktabga bormagansiz deb chiqsin
+
+// for(let item = 0; item <=100; item +=20){
+//     console.log(item);
+// }
+
+// masala 02
+
+// for (let item = 0; item <= 10; item++ ){
+//     if (item % 2 !== 0){
+//         console.log(item);
+//     }
+// }
+
+
+//masala 03
+
+// let array = ["asliddin" ,"zuxra","sultonali","yodgor","ulugbek","husan"];
+// console.log(array, length);
+
+// for(let element = 0; element , array,length ; element++ ){
+//     console.log(array[element]);
+// }
+
+
+
+
+
+
+
+
+// masala 01
+
+// let numbers = [45 ,34,45,56,78,89,90,23,12,22]
+
+// console.log("Arrayning uzunligi" , numbers.length);
+// for(let item = 0; item < numbers .length; item++){
+//     if(numbers[item] % 2 ===0){
+//         console.log(numbers[item]);
+//     }
+// }
+
+
+
+
+
+///// dars 
+
+
+
+// let city = "Husniddin"
+
+// console.log(city.length);
+// console.log(city.indexOf("i",4));
+// console.log(city.lastIndexOf("i"));
+// console.log(city.slice(0.3));
+// console.log(city.trim(0.3));
+// let city = "Munavvar Qori Abdurashidov"
+// console.log(city.slice(city.indexOf(' ')+1 , city.length) + ' ' + city.slice(0 , city.indexOf(' ') )) ;
+
+// let name = city.slice(0 ,city.indexOf(' ') )
+
+// console.log(city.slice(city.indexOf(' ') +1, city.length ));
+// console.log(name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let array = [
+//     "Asliddin" ,
+//     "Zuxra",
+//     "Sultonali",
+//     "Yodgor",
+//     "Ulugbek",
+//     "Husan"
+// ];
+// for(let item = 0; item <  array.length; item++){
+//     console.log(array[item].toUpperCase());
+// }
+
+////masala 2
+
+// let array = [
+//     "Asliddin" ,
+//     "Zuxra",
+//     "Sultonali",
+//     "Yodgor",
+//     "Ulugbek",
+//     "Husan"
+// ];
+// for(let item = 0; item <  array.length; item++){
+//  
+////ma   console.Husan. * par2) %3);}
+
+
+
+
+
+
+
+// other lesson
+
+
+
+
+// calculation(40,50)
+
+// function calculation(a,b){
+//     if (a > b){
+//         console.log("a katta");
+//     }
+//     else{
+//         console.log("b katta");
+//     }
+// }
+
+
+
+
+// example 02
+
+// let array = [
+//     "Asliddin" ,
+//     "Zuxra",
+//     "Sultonali",
+//     "Yodgor",
+//     "Ulugbek",
+//     "Husan"
+// ];
+
+// calculation(array)
+
+// function calculation(arr) {
+//     for (let item = 0; item < arr.length; item++) {
+//         console.log( arr[item].toUpperCase());
+        
+//     }
+    
+// }
+
+
+// calculation(array)
+
+// let array = [
+//     "Asliddin" ,
+//     "Zuxra",
+//     "Sultonali",
+//     "Yodgor",
+//     "Ulugbek",
+//     "Husan"
+// ];
+
+// calculation(array)
+
+// function calculation(arr) {
+//     for (let item = 0; item < arr.length; item++) {
+        
+//         if(arr[item].length > 6) 
+//         console.log(arr[item]);       
+//     }
+    
+// }
+
+//exammple 03
+
+// let array = [
+//     "Asliddin" ,
+//     "Zuxra",
+//     "Sultonali",
+//     "Yodgor",
+//     "Ulugbek",
+//     "Husan"
+// ];
+
+// let newArr =[]
+
+// function calculation(arr) {
+//     for (let item = 0; item < arr.length; item++) {
+        
+//         if(arr[item].length > 6) {
+//             newArr.push(arr[item]);
+          
+//         }     
+//     }
+    
+// }
+// calculation(array)
+
+// console.log(newArr);
+
+
+// homework 01
+
+// let array = [ 23, 34, 45, 67, 78, 29 ]
+
+// Shu arraydagi sonlar 40 dan katta bo'lganlarini boshqa yangi arrayga joylaydigan function yozing 
+
+
+// let array = [ 23, 34, 45, 67, 78, 29 ]
+
+// function calculation(array){
+//     let x = []
+//     for(let item = 0; item < array.length; item++){
+//         if(array[item]>40){
+//             x.push(
+//                 array[item]
+//             )
+//         }
+//     }
+//     console.log(x);
+// }
+
+// let x =  calculation(array);
+
+
+
+
+// masala 2
+
+// let array = [ 23, 34, 45, 67, 78, 29 ]
+
+// function calculation(array){
+//     let x = []
+//     for(let item = 0; item < array.length; item++){
+//         if(array[item] % 2 !==0){
+//             x.push(
+//                 array[item]
+//             )
+//         }
+//     }
+//     console.log(x);
+// }
+
+// let x =  calculation(array);```
+
+//masala 3
+
+// let array = [ 23, 34, 45, 67, 78, 29 ]
+
+// function calculation(array){
+//     let x = []
+//     for(let item = 0; item < array.length; item++){
+//         if(array[item] % 2 ===0){
+//             x.push(
+//                 array[item]
+//             )
+//         }
+//     }
+//     console.log(x);
+// }
+
+// let x =  calculation(array);
+
